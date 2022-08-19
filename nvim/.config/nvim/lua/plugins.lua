518f85c290e3086635b8c69282ac2d8a776f5531
bradybridges <bradyjbridges@gmail.com>
2022-08-19 10:38:22 -0600

    Add hop to plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -45,4 +45,12 @@ packer.startup(function(use)
 
     use 'lewis6991/gitsigns.nvim'
     use 'dinhhuy258/git.nvim' -- For git blame & browse
+    use {
+        'phaazon/hop.nvim',
+        branch = 'v2',
+        config = function()
+            --config here :h hop-config
+            require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+        end
+    }
 end)
