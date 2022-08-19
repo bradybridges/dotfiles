a9e5b88ca24001bf81f4282979dba1977919a4d2
bradybridges <bradyjbridges@gmail.com>
2022-08-19 10:38:03 -0600

    Add some more mappings, configure hop mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -2,18 +2,30 @@ local keymap = vim.keymap
 
 keymap.set('n', 'x', '"_x')
 
+-- Reload Config
+keymap.set('n', '<Space><Enter>', ':luafile %<CR>')
+
 -- Increment/decrement
 keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
--- Delete a word backwards
---keymap.set('n', 'dw', 'vb"_d')
-
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
+-- Copy to Clipboard
+keymap.set('v', 'Y', '"+y')
+
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+
+-- Close Tab
+keymap.set('n', 'tw', ':tabclose<CR>')
+
+-- Reload File
+keymap.set('n', 'R', ':edit!<CR>')
+
+-- Tab File
+keymap.set('n', '<F9>', ':retab!<CR>');
 
 -- Split window
 keymap.set('n', 's', '<C-w>s<C-w>j:terminal<CR>')
@@ -35,9 +47,21 @@ keymap.set('n', '<C-w><down>', '<C-w>-')
 -- Insert Mode
 keymap.set('i', 'jj', '<Esc>')
 
--- Visual Mode
-keymap.set('v', 'J', ':m \'>+4<CR>gv=gv')
-keymap.set('v', 'K', ':m \'<1<CR>gv=gv')
+-- Shift Lines
+keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
--- Terminal
+-- Find Selected
+keymap.set('v', '//', 'y/\\V<C-R>=escape(@",\'/\')<CR><CR>')
+
+-- Escape Terminal
 keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Open Terminal
+keymap.set('n', '<Space>`', ':terminal<CR>')
+
+-- Hop
+keymap.set('n', 'f',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<CR>")
+keymap.set('n', 'F',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>")
