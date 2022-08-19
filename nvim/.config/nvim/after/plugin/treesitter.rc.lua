e5a53c27fea03b0b7cabd643bd60b13360469111
bradybridges <bradyjbridges@gmail.com>
2022-08-19 15:34:13 -0600

    Add rainbow plugin/config

--- nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -2,28 +2,33 @@ local status, ts = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 ts.setup {
-    highlight = {
-        enable = true,
-        disable = {},
-    },
-    indent = {
-        enable = true,
-        disable = {},
-    },
-    ensure_installed = {
-        "tsx",
-        "json",
-        "yaml",
-        "css",
-        "scss",
-        "html",
-        "lua",
-        "javascript",
-        "typescript",
-    },
-    autotag = {
-        enable = true,
-    },
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	ensure_installed = {
+		"tsx",
+		"json",
+		"yaml",
+		"css",
+		"scss",
+		"html",
+		"lua",
+		"javascript",
+		"typescript",
+	},
+	autotag = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = false,
+		max_file_lines = 1000,
+	}
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
