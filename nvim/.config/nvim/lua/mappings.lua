dff75f7d93b639b0a4baba25747566604865791a
brady bridges <bradybridges@gmail.com>
2022-09-17 19:22:34 -0600

    Add normal + terminal split  mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,13 +1,5 @@
 local keymap = vim.keymap
 
-local function TrimWhitespaces()
-	local save = vim.fn.winsaveview()
-	vim.cmd('%s/\\\\\\@<!\\s\\+$//e')
-	vim.fn.winrestview(save)
-end
-
---keymap.set('n', 'x', '"_x')
-
 -- Reload Config
 keymap.set('n', '<Space><Enter>', ':luafile %<CR>')
 
@@ -18,26 +10,38 @@ keymap.set('n', '-', '<C-x>')
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
+-- Copy to Clipboard
+keymap.set('v', 'Y', '"+y')
+
 -- New tab
 keymap.set('n', 'te', ':tabedit')
 
--- Close current tab
+-- Close Tab
 keymap.set('n', 'tw', ':tabclose<CR>')
 
 -- Reload File
-keymap.set('n', 'R', ':edit!<CR>');
+keymap.set('n', 'R', ':edit!<CR>')
 
--- Retab File
+-- Tab File
 keymap.set('n', '<F9>', ':retab!<CR>')
 
 -- Replace Trailing Whitespaces
+local function TrimWhitespaces()
+	local save = vim.fn.winsaveview()
+	vim.cmd('%s/\\\\\\@<!\\s\\+$//e')
+	vim.fn.winrestview(save)
+end
+
 keymap.set('n', '<Space>T', TrimWhitespaces)
 
 -- Split window
-keymap.set('n', 's', '<C-w>s<C-w>j:terminal<CR>')
-keymap.set('n', 'vs', '<C-w>v<C-w>l:terminal<CR>')
+keymap.set('n', 's', '<C-w>s<C-w>j')
+keymap.set('n', 'vs', '<C-w>v<C-w>l')
+keymap.set('n', 'ts', '<C-w>s<C-w>j:terminal<CR>')
+keymap.set('n', 'tvs', '<C-w>v<C-w>l:terminal<CR>')
 
 -- Move window
+--keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('n', '<up>', '<C-w><up>')
 keymap.set('n', '<down>', '<C-w><down>')
 keymap.set('n', '<left>', '<C-w><left>')
@@ -49,18 +53,15 @@ keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
 
--- Escape Insert
+-- Insert Mode
 keymap.set('i', 'jj', '<Esc>')
 
 -- Shift Lines
 keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
 keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
--- Copy To Clipboard
-keymap.set('v', 'Y', '"+y')
-
 -- Find Selected
-keymap.set('v', '//', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>')
+keymap.set('v', '//', 'y/\\V<C-R>=escape(@",\'/\')<CR><CR>')
 
 -- Escape Terminal
 keymap.set('t', '<Esc>', '<C-\\><C-n>')
@@ -70,10 +71,10 @@ keymap.set('n', '<Space>`', ':terminal<CR>')
 
 -- Hop
 keymap.set('n', 'f',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<cr>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<CR>")
 keymap.set('n', 'F',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>")
 keymap.set('n', '<Space>s',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
 keymap.set('n', '<Space>S',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>")
