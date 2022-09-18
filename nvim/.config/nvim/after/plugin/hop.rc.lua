f2274509a1c382b63b191520ed550a98051fabd4
bradybridges <bradyjbridges@gmail.com>
2022-09-18 13:15:45 -0600

    Add setup to RC

--- nvim/.config/nvim/after/plugin/hop.rc.lua
+++ nvim/.config/nvim/after/plugin/hop.rc.lua
@@ -1,5 +1,21 @@
-local status, hop = pcall(require, 'hop')
+local status, hop = pcall(require, "hop")
 
 if (not status) then return end
 
-return hop.setup()
+local keymap = vim.keymap
+
+-- Hop
+keymap.set('n', 'f',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<CR>")
+keymap.set('n', 'F',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>")
+keymap.set('n', '<Space>s',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
+keymap.set('n', '<Space>S',
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>")
+keymap.set('n', '<Space>l', "<cmd>:HopLineStart<CR>")
+
+return hop.setup({
+	keys = 'etovxqpdygfblzhckisuran'
+})
+
