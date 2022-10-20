55607d6f66a9a6b10ad445f540572bb83f14377f
bradybridges <bradyjbridges@gmail.com>
2022-10-20 15:00:03 -0600

    Update depracated capabilities fn

--- nvim/.config/nvim/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/plugin/lspconfig.rc.lua
@@ -61,9 +61,8 @@ protocol.CompletionItemKind = {
 	'ﬦ', -- Operator
 	'', -- TypeParameter
 }
-
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
 	vim.lsp.protocol.make_client_capabilities()
 )
 
