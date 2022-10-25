4bb30c1314a1748826f067ec4460ad3d3811fe21
bradybridges <bradyjbridges@gmail.com>
2022-10-25 10:18:50 -0600

    Improve organization, add some cmp sources

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,18 +7,25 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
+	-- Colorschemes
 	use {
 		'svrana/neosolarized.nvim',
 		requires = { 'tjdevries/colorbuddy.nvim' }
 	}
+
+	-- Treesitter
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	}
+
+	-- Move anywhere in buffer
 	use {
 		'phaazon/hop.nvim',
 		branch = 'v2',
 	}
+
+	-- Surround
 	use({
 		"kylechui/nvim-surround",
 		requires = { 'nvim-treesitter/nvim-treesitter-textobjects' },
@@ -29,31 +36,49 @@ packer.startup(function(use)
 			})
 		end
 	})
+
+	-- Package Manager
 	use 'wbthomason/packer.nvim' -- Plugin Manager
+
+	-- UI
+	use 'p00f/nvim-ts-rainbow' -- Rainbow brackets
+	use 'lukas-reineke/indent-blankline.nvim' -- Indentation highlights
+	use 'akinsho/nvim-bufferline.lua'
+	use 'numToStr/FTerm.nvim' -- Floating Terminal
 	use 'nvim-lualine/lualine.nvim' -- Statusline
-	use 'nvim-lua/plenary.nvim' -- Common utilities
-	use 'onsails/lspkind-nvim' -- vscode-like pictograms
-	use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-	use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-	use 'hrsh7th/nvim-cmp' -- Completion
-	use 'neovim/nvim-lspconfig' -- LSP
-	use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+	use 'folke/zen-mode.nvim'
 	use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
 	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'glepnir/lspsaga.nvim' -- LSP UIs
-	use 'L3MON4D3/LuaSnip'
 	use 'kyazdani42/nvim-web-devicons' -- File icons
-	use 'nvim-telescope/telescope.nvim'
-	use 'nvim-telescope/telescope-file-browser.nvim'
 	use 'windwp/nvim-autopairs'
 	use 'windwp/nvim-ts-autotag'
 	use 'norcalli/nvim-colorizer.lua'
-	use 'folke/zen-mode.nvim'
-	use 'akinsho/nvim-bufferline.lua'
+	use 'jose-elias-alvarez/null-ls.nvim'
+
+	-- Git
 	use 'lewis6991/gitsigns.nvim'
 	use 'dinhhuy258/git.nvim' -- For git blame & browse
-	use 'p00f/nvim-ts-rainbow' -- Rainbow brackets
-	use 'lukas-reineke/indent-blankline.nvim' -- Indentation highlights
-	use 'numToStr/FTerm.nvim' -- Floating Terminal
+
+	-- Snippet
+	use 'L3MON4D3/LuaSnip'
+	use 'rafamadriz/friendly-snippets'
+
+	-- Telescope
+	use 'nvim-telescope/telescope.nvim'
+	use 'nvim-telescope/telescope-file-browser.nvim'
+
+	-- LSP
+	use 'neovim/nvim-lspconfig' -- LSP
+	use 'onsails/lspkind-nvim' -- vscode-like pictograms
+	use 'williamboman/mason-lspconfig.nvim'
+	use 'glepnir/lspsaga.nvim' -- LSP UIs
+	use 'nvim-lua/plenary.nvim' -- Common utilities
+
+	-- CMP
+	use 'hrsh7th/nvim-cmp' -- Completion
+	use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+	use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+	use 'hrsh7th/cmp-nvim-lua' -- nvim-cmp source for lua
+	use 'hrsh7th/cmp-path' -- nvim-cmp source for path
+
 end)
