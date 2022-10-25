349ce6a3f621ec5a737af41382e946ef65bb6a81
bradybridges <bradyjbridges@gmail.com>
2022-10-25 10:15:23 -0600

    Formatting, add opts, remove active highlights

--- nvim/.config/nvim/after/plugin/bufferline.rc.lua
+++ nvim/.config/nvim/after/plugin/bufferline.rc.lua
@@ -2,15 +2,16 @@ local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
 bufferline.setup({
-    options = {
-        mode = "tabs",
-        separator_style = 'slant',
-        always_show_bufferline = false,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true
-    },
+	options = {
+		mode = "tabs",
+		separator_style = 'slant',
+		always_show_bufferline = false,
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		color_icons = true
+	},
 })
 
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+vim.cmd [[highlight BufferLineBufferSelected none]]
