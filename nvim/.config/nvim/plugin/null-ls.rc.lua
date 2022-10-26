8255405d2d70aef7a01e1644cff59bf5f64b1eee
brady bridges <bradybridges@gmail.com>
2022-10-25 23:33:29 -0600

    Add markdown formatting config

--- nvim/.config/nvim/plugin/null-ls.rc.lua
+++ nvim/.config/nvim/plugin/null-ls.rc.lua
@@ -3,8 +3,23 @@ if (not status) then return end
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.diagnostics.eslint_d.with ({
+		null_ls.builtins.diagnostics.eslint_d.with({
 			diagnostics_format = '[eslint] #{m}\n(#{c})'
 		}),
 	},
 })
+
+local markdownlint = {
+	name = "markdownlint",
+	method = null_ls.methods.FORMATTING,
+	generator = null_ls.formatter({
+		command = "markdownlint",
+		args = { "--fix", "$FILENAME" },
+		-- doesn't support stdin for fixing, but we can use a temp file
+		to_temp_file = true,
+	}),
+	filetypes = { "markdown" },
+}
+
+-- register separately or add to other sources
+null_ls.register(markdownlint)
