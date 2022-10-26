bf443563deaf0f58384398cad8308294ace62138
brady bridges <bradybridges@gmail.com>
2022-10-25 23:20:50 -0600

    Formatting

--- nvim/.config/nvim/after/plugin/hop.rc.lua
+++ nvim/.config/nvim/after/plugin/hop.rc.lua
@@ -3,17 +3,22 @@ local status, hop = pcall(require, "hop")
 if (not status) then return end
 
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Hop
 keymap.set('n', 'f',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<CR>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<CR>"
+	, opts)
 keymap.set('n', 'F',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>"
+	, opts)
 keymap.set('n', '<Space>s',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
+	, opts)
 keymap.set('n', '<Space>S',
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>")
-keymap.set('n', '<Space>l', "<cmd>:HopLineStart<CR>")
+	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>"
+	, opts)
+keymap.set('n', '<Space>l', "<cmd>:HopLineStart<CR>", opts)
 
 return hop.setup({
 	keys = 'etovxqpdygfblzhckisuran'
