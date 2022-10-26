098fa5dd42dd9dbe29af331f08aa2bc9a378dcba
brady bridges <bradybridges@gmail.com>
2022-10-25 23:30:00 -0600

    Use lang tree and old school vim regex highlighting, add commenting, update rainbow config

--- nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -2,33 +2,29 @@ local status, ts = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 ts.setup {
+	ensure_installed = 'all',
 	highlight = {
 		enable = true,
 		disable = {},
+		use_langaugetree = true,
+		additional_vim_regex_highlighting = true,
 	},
 	indent = {
 		enable = true,
 		disable = {},
-	},
-	ensure_installed = {
-		"tsx",
-		"typescript",
-		"json",
-		"yaml",
-		"css",
-		"html",
-		"lua",
-		"javascript",
-
 	},
 	autotag = {
 		enable = true,
 	},
 	rainbow = {
 		enable = true,
-		extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = 1000, -- Do not enable for files with more than n lines, int
-	}
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
