00736240073791e919cd4cc69a7ddd48c86b488b
brady bridges <bradybridges@gmail.com>
2022-12-25 17:59:38 -0600

    Update harpoon prev/next mappings to use leader

--- nvim/.config/nvim/after/plugin/harpoon.rc.lua
+++ nvim/.config/nvim/after/plugin/harpoon.rc.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 
-keymap.set('n', '<Space>a', "<cmd>lua require'harpoon.mark'.add_file()<CR>")
+keymap.set('n', '<Leader>a', "<cmd>lua require'harpoon.mark'.add_file()<CR>")
 keymap.set('n', ';m', "<cmd>lua require'harpoon.ui'.toggle_quick_menu()<CR>")
-keymap.set('n', 'hn', "<cmd>lua require'harpoon.ui'.nav_next()<CR>")
-keymap.set('n', 'hp', "<cmd>lua require'harpoon.ui'.nav_prev()<CR>")
+keymap.set('n', '<Leader>hn', "<cmd>lua require'harpoon.ui'.nav_next()<CR>")
+keymap.set('n', '<Leader>hp', "<cmd>lua require'harpoon.ui'.nav_prev()<CR>")
