23fffbb00f14952a3cc77f336509fd24fd2db22f
brady bridges <bradybridges@gmail.com>
2022-12-25 17:59:56 -0600

    Add lazygit fTerm function + mapping

--- nvim/.config/nvim/after/plugin/f-term.rc.lua
+++ nvim/.config/nvim/after/plugin/f-term.rc.lua
@@ -10,4 +10,15 @@ fTerm.setup({
 	},
 })
 
-vim.keymap.set('n', '<Space>`', '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+-- Lazygit floating terminal
+local lg = fTerm:new({
+	cmd = "lazygit"
+})
+
+-- Toggle fTerm
+vim.keymap.set('n', ';`', '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+
+ -- toggle fTerm lazygit terminal
+vim.keymap.set('n', ';l', function()
+	lg:toggle()
+end)
