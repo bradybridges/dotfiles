83240970651573e9539d34796ed575d20ba4fd0b
bradybridges <bradyjbridges@gmail.com>
2023-01-17 11:35:57 -0700

    Convert lspsaga to use new setup fn

--- nvim/.config/nvim/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/plugin/lspsaga.rc.lua
@@ -1,11 +1,11 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.init_lsp_saga {
+saga.setup({
 	server_filetype_map = {
 		typescript = 'typescript'
 	}
-}
+})
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-q>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
