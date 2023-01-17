e46546f254e4d03d8aa340ecd093e09f4f5ec5e5
bradybridges <bradyjbridges@gmail.com>
2023-01-17 11:35:14 -0700

    Update autopair config

--- nvim/.config/nvim/after/plugin/autopair.rc.lua
+++ nvim/.config/nvim/after/plugin/autopair.rc.lua
@@ -1,6 +1,19 @@
-local status, autopairs = pcall(require, "nvim-autopairs")
+local status, autopairs, cmp_autopairs, cmp = pcall(function()
+	return require "nvim-autopairs",
+		require "nvim-autopairs.completion.cmp",
+		require "cmp"
+end)
+
 if (not status) then return end
 
+-- make autopairs and completion work together
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
 autopairs.setup({
-    disable_filetype = { "TelescopePrompt", "vim" },
+	check_ts = true, -- enable treesitter
+	disable_filetype = { "TelescopePrompt", "vim" },
+	ts_config = {
+		lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+		javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+	},
 })
