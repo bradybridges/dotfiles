a8d077d9dc725d3b8d95717282ec16820da4947e
bradybridges <bradyjbridges@gmail.com>
2023-01-17 11:35:26 -0700

    Fomratting prettier config

--- nvim/.config/nvim/after/plugin/prettier.rc.lua
+++ nvim/.config/nvim/after/plugin/prettier.rc.lua
@@ -2,14 +2,14 @@ local status, prettier = pcall(require, "prettier")
 if (not status) then return end
 
 prettier.setup {
-    bin = 'prettierd',
-    filetypes = {
-        "css",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "json",
-        "scss",
-        "less"
-    }
+	bin = 'prettierd',
+	filetypes = {
+		"css",
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"json",
+		"scss",
+		"less"
+	}
 }
