bdeeff81b7ee20bfff6dc82a965d803964284b44
brady bridges <bradybridges@gmail.com>
2023-01-28 21:40:36 -0700

    Turn off additional regex highlighting

--- nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -7,7 +7,7 @@ ts.setup {
 	highlight = {
 		enable = true,
 		use_languagetree = true,
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	indent = {
 		enable = true,
