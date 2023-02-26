4a6f5826692abfd7a505f2827d234b794405deb9
brady bridges <bradybridges@gmail.com>
2023-02-26 14:59:43 -0700

    Ignore node modules in telescope

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -12,7 +12,7 @@ local fb_actions = require "telescope".extensions.file_browser.actions
 telescope.setup {
 	defaults = {
 		prompt_prefix = " ",
-
+		file_ignore_patterns = { "node_modules" },
 		mappings = {
 			n = {
 				["q"] = actions.close,
