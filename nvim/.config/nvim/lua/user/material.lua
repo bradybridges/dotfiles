ca232ce98ef905dc0555ad13e3ea95c3ac0fb7b4
bradybridges <bradyjbridges@gmail.com>
2023-03-02 15:18:12 -0700

    Add material colorscheme configuration + overrides

---
+++ nvim/.config/nvim/lua/user/material.lua
@@ -0,0 +1,25 @@
+local material_ok, material = pcall(require, 'material')
+
+if not material_ok then return end
+
+material.setup({
+	custom_highlights = {
+		Comment = { fg = "#999999" },
+		CursorLine = { bg = "none" },
+		Cursor = { bg = "#999999" }
+	},
+	plugins = { -- Uncomment the plugins that you use to highlight them
+		"gitsigns",
+		"hop",
+		"indent-blankline",
+		"nvim-cmp",
+		"nvim-tree",
+		"nvim-web-devicons",
+		"telescope",
+		"which-key",
+	},
+	disable = {
+		background = false,
+	},
+	lualine_style = "stealth"
+})
