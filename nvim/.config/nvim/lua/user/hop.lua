80520ec737c6be34fbe057b2516c28b2908094b4
brady bridges <bradybridges@gmail.com>
2023-03-02 00:46:01 -0700

    Comment out conflicting mapping

--- nvim/.config/nvim/lua/user/hop.lua
+++ nvim/.config/nvim/lua/user/hop.lua
@@ -17,11 +17,11 @@ keymap.set(
 	"F",
 	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<CR>"
 )
-keymap.set(
-	"n",
-	"<Leader>s",
-	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
-)
+-- keymap.set(
+-- 	"n",
+-- 	"<Leader>s",
+-- 	"<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
+-- )
 keymap.set(
 	"n",
 	"<Leader>S",
