4dc8571e8f1b8ed694900747eabd096774d04f83
brady bridges <bradybridges@gmail.com>
2023-03-02 01:18:43 -0700

    Switch to material colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,5 @@
-local colorscheme = "tokyonight"
+local colorscheme = "material"
+vim.g.material_style = "deep ocean"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
