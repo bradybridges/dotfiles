dc30d5098f0c46980dff8c4bf878cc52b02721cc
bradybridges <bradyjbridges@gmail.com>
2023-03-08 16:25:17 -0700

    Fix comment.nvim for TSX/JSX files

--- nvim/.config/nvim/lua/user/comment.lua
+++ nvim/.config/nvim/lua/user/comment.lua
@@ -5,28 +5,26 @@ end
 
 comment.setup({
 	pre_hook = function(ctx)
-		local U = require("Comment.utils")
+		local U = require "Comment.utils"
 
-		local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
-		if not status_utils_ok then
-			return
-		end
+		local utils_ok, ts_utils = pcall(require, 'ts_context_commentstring.utils')
+
+		if not utils_ok then return end
 
 		local location = nil
 		if ctx.ctype == U.ctype.block then
-			location = utils.get_cursor_location()
+			location = ts_utils.get_cursor_location()
 		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-			location = utils.get_visual_start_location()
+			location = ts_utils.get_visual_start_location()
 		end
 
-		local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
-		if not status_internals_ok then
-			return
-		end
+		local internal_ok, ts_internal = pcall(require, 'ts_context_commentstring.internal')
 
-		return internals.calculate_commentstring({
+		if not internal_ok then return end
+
+		return ts_internal.calculate_commentstring {
 			key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
 			location = location,
-		})
+		}
 	end,
 })
--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -16,14 +16,18 @@ configs.setup({
 		"yaml",
 		"markdown",
 		"markdown_inline",
-	}, -- one of "all" or a list of languages
+	},                          -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		enable = true,          -- false will disable the whole extension
+		disable = { "css" },    -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
