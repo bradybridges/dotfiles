e298ff0f3f5fdb87666b33de60ab15fd6092bf47
bradybridges <bradyjbridges@gmail.com>
2023-03-13 17:25:20 -0500

    Add git blame plugin

---
+++ nvim/.config/nvim/lua/user/git-blame.lua
@@ -0,0 +1,3 @@
+vim.cmd [[
+	let g:gitblame_enabled = 0
+]]
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -56,11 +56,11 @@ return packer.startup(function(use)
 	use({ "lukas-reineke/indent-blankline.nvim" })
 	use({ "goolord/alpha-nvim" })
 	use({ "folke/which-key.nvim" })
-	use({ "folke/zen-mode.nvim" }) -- Focus on window
+	use({ "folke/zen-mode.nvim" })          -- Focus on window
 	use({ "windwp/nvim-ts-autotag" })
 	use({ "phaazon/hop.nvim", branch = "v2" }) -- Move anywhere in buffer
-	use({ "xiyaowong/nvim-transparent" }) -- Toggle nvim transparency
-	use({ "norcalli/nvim-colorizer.lua" }) -- Colorize hex & colors
+	use({ "xiyaowong/nvim-transparent" })   -- Toggle nvim transparency
+	use({ "norcalli/nvim-colorizer.lua" })  -- Colorize hex & colors
 
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim" })
@@ -69,23 +69,23 @@ return packer.startup(function(use)
 	use({ "marko-cerovac/material.nvim" })
 
 	-- Cmp
-	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-	use({ "hrsh7th/cmp-path" }) -- path completions
+	use({ "hrsh7th/nvim-cmp" })      -- The completion plugin
+	use({ "hrsh7th/cmp-buffer" })    -- buffer completions
+	use({ "hrsh7th/cmp-path" })      -- path completions
 	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
 	use({ "hrsh7th/cmp-nvim-lsp" })
 	use({ "hrsh7th/cmp-nvim-lua" })
 
 	-- Snippets
-	use({ "L3MON4D3/LuaSnip" }) --snippet engine
+	use({ "L3MON4D3/LuaSnip" })          --snippet engine
 	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
 	-- LSP
-	use({ "neovim/nvim-lspconfig" }) -- enable LSP
-	use({ "williamboman/mason.nvim" }) -- simple to use language server installer
+	use({ "neovim/nvim-lspconfig" })        -- enable LSP
+	use({ "williamboman/mason.nvim" })      -- simple to use language server installer
 	use({ "williamboman/mason-lspconfig.nvim" })
 	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
-	use({ "RRethy/vim-illuminate" }) -- Hightlight instances
+	use({ "RRethy/vim-illuminate" })        -- Hightlight instances
 	use({ "akinsho/flutter-tools.nvim" })
 
 	-- Telescope
@@ -98,6 +98,7 @@ return packer.startup(function(use)
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim" })
+	use({ "f-person/git-blame.nvim" })
 
 	-- Surround
 	use({
