0bff300218becdb3fd5cc46fa8c4ec4de525e706
bradybridges <bradyjbridges@gmail.com>
2023-03-20 12:11:51 -0600

    Update alpha menu to remove un-used stuff, update mappings to reconcile conflicts

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -13,12 +13,10 @@ dashboard.section.header.val = {
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("s", "  Sync Plugins", ":PackerSync<CR>"),
+	dashboard.button("sp", "  Sync Plugins", ":PackerSync<CR>"),
 	dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
