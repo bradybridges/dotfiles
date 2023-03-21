314380f2a8770b7414e9ce0ecff09a023dde3067
bradybridges <bradyjbridges@gmail.com>
2023-03-20 20:11:30 -0600

    Add telescope workspace diagnostics mapping

--- nvim/.config/nvim/lua/user/whichkey.lua
+++ nvim/.config/nvim/lua/user/whichkey.lua
@@ -84,6 +84,7 @@ local mappings = {
 		"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
 		"Buffers",
 	},
+	["d"] = { "<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics" },
 	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
 	["w"] = { "<cmd>w!<CR>", "Save" },
 	["q"] = { "<cmd>q!<CR>", "Quit" },
