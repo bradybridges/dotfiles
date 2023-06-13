0d9c7167a80d719aba10df37c152174f88cdf15d
brady bridges <bradybridges@gmail.com>
2023-06-13 00:14:56 -0500

    Add mapping to toggle GPT

--- nvim/.config/nvim/lua/user/whichkey.lua
+++ nvim/.config/nvim/lua/user/whichkey.lua
@@ -19,8 +19,8 @@ local setup = {
 			text_objects = true, -- help for text objects triggered after entering an operator
 			windows = true, -- default bindings on <c-w>
 			nav = true, -- misc bindings to work with windows
-			z = true,   -- bindings for folds, spelling and others prefixed with z
-			g = true,   -- bindings for prefixed with g
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
 		},
 	},
 	-- add operators that will trigger motion and text object completion
@@ -50,15 +50,15 @@ local setup = {
 		winblend = 0,
 	},
 	layout = {
-		height = { min = 4, max = 25 },                                        -- min and max height of the columns
-		width = { min = 20, max = 50 },                                        -- min and max width of the columns
-		spacing = 3,                                                           -- spacing between columns
-		align = "left",                                                        -- align columns left, center or right
+		height = { min = 4, max = 25 }, -- min and max height of the columns
+		width = { min = 20, max = 50 }, -- min and max width of the columns
+		spacing = 3, -- spacing between columns
+		align = "left", -- align columns left, center or right
 	},
-	ignore_missing = true,                                                     -- enable this to hide mappings for which you didn't specify a label
+	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-	show_help = true,                                                          -- show help message on the command line when the popup is visible
-	triggers = "auto",                                                         -- automatically setup triggers
+	show_help = true, -- show help message on the command line when the popup is visible
+	triggers = "auto", -- automatically setup triggers
 	-- triggers = {"<leader>"} -- or specify a list manually
 	triggers_blacklist = {
 		-- list of mode / prefixes that should never be hooked by WhichKey
@@ -70,7 +70,7 @@ local setup = {
 }
 
 local opts = {
-	mode = "n",  -- NORMAL mode
+	mode = "n", -- NORMAL mode
 	prefix = "<leader>",
 	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
 	silent = true, -- use `silent` when creating keymaps
@@ -175,6 +175,7 @@ local mappings = {
 		R = { "<cmd>Telescope registers<cr>", "Registers" },
 		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
 		C = { "<cmd>Telescope commands<cr>", "Commands" },
+		a = { "<cmd>ChatGPT<cr>", "ChatGPT" },
 	},
 	t = {
 		name = "Terminal",
@@ -184,7 +185,7 @@ local mappings = {
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 		c = { "<cmd>lua _CYPRESS_TOGGLE()<cr>", "Cypress" },
 		s = { "<cmd>lua _HEADLESS_TOGGLE()<cr>", "Headless Cypress" },
-		o = { "<cmd>lua _VS_TOGGLE()<cr>", "VS Code"},
+		o = { "<cmd>lua _VS_TOGGLE()<cr>", "VS Code" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
