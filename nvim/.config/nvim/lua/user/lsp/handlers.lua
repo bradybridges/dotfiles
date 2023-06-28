932371648513f7fa183b708200930b534df7ff11
brady bridges <bradybridges@gmail.com>
2023-06-27 22:51:59 -0500

    Update icons

--- nvim/.config/nvim/lua/user/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/lsp/handlers.lua
@@ -12,10 +12,10 @@ M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 M.setup = function()
 	local signs = {
 
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn",  text = "" },
-		{ name = "DiagnosticSignHint",  text = "" },
-		{ name = "DiagnosticSignInfo",  text = "" },
+		{ name = "DiagnosticSignError", text = "" },
+		{ name = "DiagnosticSignWarn", text = "" },
+		{ name = "DiagnosticSignHint", text = "" },
+		{ name = "DiagnosticSignInfo", text = "" },
 	}
 
 	for _, sign in ipairs(signs) do
@@ -23,9 +23,9 @@ M.setup = function()
 	end
 
 	local config = {
-		virtual_text = { spacing = 4, prefix = "" }, -- disable virtual text
+		virtual_text = { spacing = 4, prefix = " " }, -- disable virtual text
 		signs = {
-			active = signs,                     -- show signs
+			active = signs, -- show signs
 		},
 		update_in_insert = true,
 		underline = true,
