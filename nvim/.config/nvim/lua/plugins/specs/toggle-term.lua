4b2ba7c1a6fb30d99d31dc7882c3287973142f99
brady bridges <bradybridges@gmail.com>
2023-07-18 01:47:53 -0500

    Update cmd => event

--- nvim/.config/nvim/lua/plugins/specs/toggle-term.lua
+++ nvim/.config/nvim/lua/plugins/specs/toggle-term.lua
@@ -1,6 +1,6 @@
 return {
 	"akinsho/toggleterm.nvim",
-	lazy = false,
+	event = "VeryLazy",
 	config = function()
 		local status_ok, toggleterm = pcall(require, "toggleterm")
 		if not status_ok then
