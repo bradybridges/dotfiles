043bf3b5e46998a66edaeced77aaf020a1db6dfb
brady bridges <bradybridges@gmail.com>
2023-07-20 00:40:23 -0500

    Update imports after updating plugins directory structure

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,16 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "plugins.specs" },
+	{ import = "plugins.specs.treesitter" },
+	{ import = "plugins.specs.lsp" },
+	{ import = "plugins.specs.cmp" },
+	{ import = "plugins.specs.editor" },
+	{ import = "plugins.specs.ui" },
+	{ import = "plugins.specs.colorschemes" },
+	{ import = "plugins.specs.utils" },
 })
