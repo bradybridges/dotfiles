a385b3fd666fd5e2f2e2377ee0e76b359f04071f
brady bridges <bradybridges@gmail.com>
2023-07-20 00:40:00 -0500

    Add some more plugins

---
+++ nvim/.config/nvim/lua/plugins/specs/ui/lualine.lua
@@ -0,0 +1,86 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	config = function()
+		local status_ok, lualine = pcall(require, "lualine")
+		if not status_ok then
+			return
+		end
+
+		local hide_in_width = function()
+			return vim.fn.winwidth(0) > 80
+		end
+
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " " },
+			colored = true,
+			update_in_insert = false,
+			always_visible = false,
+		}
+
+		local diff = {
+			"diff",
+			colored = true,
+			symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+			-- cond = hide_in_width
+		}
+
+		local mode = {
+			"mode",
+			fmt = function(str)
+				return "-- " .. str .. " --"
+			end,
+		}
+
+		local branch = {
+			"branch",
+			icons_enabled = true,
+			icon = "",
+		}
+
+		lualine.setup({
+			options = {
+				icons_enabled = true,
+				theme = "catppuccin",
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { branch, diagnostics },
+				lualine_b = { mode },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
+				-- lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { diff, "encoding", "filetype" },
+				lualine_y = { "location", "searchcount" },
+				lualine_z = { "progress" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
+				lualine_x = {},
+				lualine_y = { "searchcount" },
+				lualine_z = { "hostname" },
+			},
+			tabline = {},
+			extensions = {},
+		})
+	end,
+}
