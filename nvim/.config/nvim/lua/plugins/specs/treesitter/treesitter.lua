aef07ef6f4f5050039b6a306f89f8418c66af22f
brady bridges <bradybridges@gmail.com>
2023-07-20 00:39:37 -0500

    Update treesitter config

---
+++ nvim/.config/nvim/lua/plugins/specs/treesitter/treesitter.lua
@@ -0,0 +1,68 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	cmd = { "TSUpdateSync" },
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			init = function() end,
+		},
+	},
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+		highlight = {
+			enable = true, -- false will disable the whole extension
+			disable = { "css" }, -- list of language that will be disabled
+		},
+		autopairs = {
+			enable = true,
+		},
+		indent = { enable = true, disable = { "python", "css" } },
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+		config = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				---@type table<string, boolean>
+				local added = {}
+				opts.ensure_installed = vim.tbl_filter(function(lang)
+					if added[lang] then
+						return false
+					end
+					added[lang] = true
+					return true
+				end, opts.ensure_installed)
+			end
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+}
