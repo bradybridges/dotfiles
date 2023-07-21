333e7c0a610e53a7ca92fb1884e57240706d8274
bradybridges <bradyjbridges@gmail.com>
2023-07-21 12:04:37 -0500

    Fix colorizer config

--- nvim/.config/nvim/lua/plugins/specs/editor/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/colorizer.lua
@@ -1,1 +1,11 @@
-return { "norcalli/nvim-colorizer.lua" }
+return {
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		local colorizer_ok, colorizer = pcall(require, 'colorizer')
+
+		if not colorizer_ok then return end
+
+		colorizer.setup()
+	end
+}
+
