259c4f04f84775e7973edf946446aac842995408
brady bridges <bradybridges@gmail.com>
2023-08-13 18:15:22 -0500

    Comment out un-used variable

--- nvim/.config/nvim/lua/plugins/specs/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/specs/lsp/null-ls.lua
@@ -9,7 +9,7 @@ return {
 		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 		local formatting = null_ls.builtins.formatting
 		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-		local diagnostics = null_ls.builtins.diagnostics
+		--[[ local diagnostics = null_ls.builtins.diagnostics ]]
 
 		null_ls.setup({
 			debug = false,
