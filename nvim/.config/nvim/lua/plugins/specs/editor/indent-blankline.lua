bb10133ac34f1194a87d6849c9893a3720bc8263
bradybridges <bradyjbridges@gmail.com>
2023-10-03 13:07:29 -0500

    Revert to v2

--- nvim/.config/nvim/lua/plugins/specs/editor/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/indent-blankline.lua
@@ -1,25 +1,58 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufReadPost", "BufNewFile" },
+	commit = "3ad57e569d1c47cf4fa2308f555003e0a5509204",
 	config = function()
-		local status_ok, ibl = pcall(require, "ibl")
+		local status_ok, indent_blankline = pcall(require, "indent_blankline")
 		if not status_ok then
 			return
 		end
 
-		local hooks_status, hooks = pcall(require, "ibl.hooks")
+		vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+		vim.g.indent_blankline_filetype_exclude = {
+			"help",
+			"startify",
+			"dashboard",
+			"packer",
+			"neogitstatus",
+			"NvimTree",
+			"Trouble",
+		}
+		vim.g.indentLine_enabled = 1
+		vim.g.indent_blankline_char = "▏"
+		vim.g.indent_blankline_show_trailing_blankline_indent = false
+		vim.g.indent_blankline_show_first_indent_level = true
+		vim.g.indent_blankline_use_treesitter = true
+		vim.g.indent_blankline_show_current_context = true
+		vim.g.indent_blankline_context_patterns = {
+			"class",
+			"return",
+			"function",
+			"method",
+			"^if",
+			"^while",
+			"jsx_element",
+			"^for",
+			"^object",
+			"^table",
+			"block",
+			"arguments",
+			"if_statement",
+			"else_clause",
+			"jsx_element",
+			"jsx_self_closing_element",
+			"try_statement",
+			"catch_clause",
+			"import_statement",
+			"operation_type",
+		}
 
-		if hooks_status then
-			hooks.register(hooks.type.ACTIVE, function(bufnr)
-				return vim.api.nvim_buf_line_count(bufnr) < 5000
-			end)
-		end
+		-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+		vim.wo.colorcolumn = "99999"
 
-		ibl.setup({
-			exclude = {
-				filetypes = { "NvimTree" },
-			},
+		indent_blankline.setup({
+			show_current_context = true,
 		})
 	end,
 }
+
