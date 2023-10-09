3d71f057e8ffa036b4db102f3d2171b1596c9c96
brady bridges <bradybridges@gmail.com>
2023-10-08 19:47:52 -0500

    Replace eob ~ with space

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -55,6 +55,9 @@ vim.opt.iskeyword:append("-")                         -- hyphenated words recogn
 vim.opt.formatoptions:remove({ "c", "r", "o" })       -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- separate vim plugins from neovim in case vim still in use
 
+-- relace eob ~ with space
+vim.opt.fillchars:append("eob: ")
+
 -- No line numbers in terminal windows
 vim.api.nvim_create_autocmd("TermOpen", {
 	callback = function()
