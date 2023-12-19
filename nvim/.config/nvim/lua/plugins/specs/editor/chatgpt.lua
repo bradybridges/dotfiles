be1d2ea21e665888a5adc629c1a55be346f44627
bradybridges <bradyjbridges@gmail.com>
2023-12-19 10:20:51 -0600

    Remove commit pin, update config to use defaults for reference

--- nvim/.config/nvim/lua/plugins/specs/editor/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/chatgpt.lua
@@ -1,6 +1,5 @@
 return {
 	"jackMort/ChatGPT.nvim",
-	commit = "f189c51d03316b4ab02766c5fed6f876f5d57cbb",
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"nvim-lua/plenary.nvim",
@@ -14,26 +13,40 @@ return {
 			return
 		end
 
+		local WELCOME_MESSAGE = [[
+
+			If you don't ask the right questions,
+				you don't get the right answers.
+											  ~ Robert Half
+		]]
+
 		gpt.setup({
 			api_key_cmd = nil,
 			yank_register = "+",
 			edit_with_instructions = {
 				diff = false,
 				keymaps = {
+					close = "<C-c>",
 					accept = "<C-y>",
 					toggle_diff = "<C-d>",
 					toggle_settings = "<C-o>",
+					toggle_help = "<C-h>",
 					cycle_windows = "<Tab>",
 					use_output_as_input = "<C-i>",
 				},
 			},
 			chat = {
-				welcome_message = "Ask and ye shall receive...",
+				welcome_message = WELCOME_MESSAGE,
 				loading_text = "Loading, please wait ...",
-				question_sign = "",
-				answer_sign = "ﮧ",
-				max_line_length = 500,
+				question_sign = "", -- 🙂
+				answer_sign = "ﮧ", -- 🤖
+				border_left_sign = "",
+				border_right_sign = "",
+				max_line_length = 120,
 				sessions_window = {
+					active_sign = "  ",
+					inactive_sign = "  ",
+					current_line_sign = "",
 					border = {
 						style = "rounded",
 						text = {
@@ -45,7 +58,7 @@ return {
 					},
 				},
 				keymaps = {
-					close = { "<C-c>", "<Esc>" },
+					close = "<C-c>",
 					yank_last = "<C-y>",
 					yank_last_code = "<C-k>",
 					scroll_up = "<C-u>",
@@ -53,13 +66,20 @@ return {
 					new_session = "<C-n>",
 					cycle_windows = "<Tab>",
 					cycle_modes = "<C-f>",
+					next_message = "<C-j>",
+					prev_message = "<C-k>",
 					select_session = "<Space>",
 					rename_session = "r",
 					delete_session = "d",
-					draft_message = "<C-d>",
+					draft_message = "<C-r>",
+					edit_message = "e",
+					delete_message = "d",
 					toggle_settings = "<C-o>",
+					toggle_sessions = "<C-p>",
+					toggle_help = "<C-h>",
 					toggle_message_role = "<C-r>",
 					toggle_system_role_open = "<C-s>",
+					stop_generating = "<C-x>",
 				},
 			},
 			popup_layout = {
@@ -121,8 +141,10 @@ return {
 				},
 				submit = "<C-Enter>",
 				submit_n = "<Enter>",
+				max_visible_lines = 20,
 			},
 			settings_window = {
+				setting_sign = "  ",
 				border = {
 					style = "rounded",
 					text = {
@@ -133,24 +155,43 @@ return {
 					winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
 				},
 			},
+			help_window = {
+				setting_sign = "  ",
+				border = {
+					style = "rounded",
+					text = {
+						top = " Help ",
+					},
+				},
+				win_options = {
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+				},
+			},
 			openai_params = {
 				model = "gpt-3.5-turbo",
 				frequency_penalty = 0,
 				presence_penalty = 0,
-				max_tokens = 500,
+				max_tokens = 300,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
 			},
 			openai_edit_params = {
-				model = "code-davinci-edit-001",
+				model = "gpt-3.5-turbo",
+				frequency_penalty = 0,
+				presence_penalty = 0,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
 			},
+			use_openai_functions_for_edits = false,
 			actions_paths = {},
 			show_quickfixes_cmd = "Trouble quickfix",
 			predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+			highlights = {
+				help_key = "@symbol",
+				help_description = "@comment",
+			},
 		})
 	end,
 }
