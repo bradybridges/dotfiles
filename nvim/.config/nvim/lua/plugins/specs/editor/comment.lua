77305d15a6a3345bf611ff43aabdd06272ad9086
brady bridges <bradybridges@gmail.com>
2024-01-02 21:16:26 -0600

    Revert "Unpin and update config"
    This reverts commit 673aaf5f98ed5455f5a47929dc532e24ee052c41.

--- nvim/.config/nvim/lua/plugins/specs/editor/comment.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/comment.lua
@@ -1,19 +1,40 @@
 return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	opts = {},
 	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
+		{ "JoosepAlviste/nvim-ts-context-commentstring", commit = "92e688f013c69f90c9bbd596019ec10235bc51de" },
 	},
 	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
+		local status_ok, comment = pcall(require, "Comment")
+		if not status_ok then
+			return
+		end
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
 		comment.setup({
-			-- for commenting tsx and jsx files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = function(ctx)
+				local U = require "Comment.utils"
+
+				local utils_ok, ts_utils = pcall(require, 'ts_context_commentstring.utils')
+
+				if not utils_ok then return end
+
+				local location = nil
+				if ctx.ctype == U.ctype.block then
+					location = ts_utils.get_cursor_location()
+				elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+					location = ts_utils.get_visual_start_location()
+				end
+
+				local internal_ok, ts_internal = pcall(require, 'ts_context_commentstring.internal')
+
+				if not internal_ok then return end
+
+				return ts_internal.calculate_commentstring {
+					key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+					location = location,
+				}
+			end,
 		})
-	end,
+	end
 }
