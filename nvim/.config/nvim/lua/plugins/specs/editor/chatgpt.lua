4ae145bb9a3759bbf9f48b3753b32f31b651544c
bradybridges <bradyjbridges@gmail.com>
2024-01-30 09:43:44 -0600

    Increase max_tokens parameter

--- nvim/.config/nvim/lua/plugins/specs/editor/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/chatgpt.lua
@@ -171,7 +171,7 @@ return {
 				model = "gpt-3.5-turbo",
 				frequency_penalty = 0,
 				presence_penalty = 0,
-				max_tokens = 300,
+				max_tokens = 2000,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
