714e87523f34e5ea01074242971f7ef73bf58117
brady bridges <bradyjbridges@gmail.com>
2024-05-21 21:23:17 -0500

    Remove format keybind

--- nvim/.config/nvim/lua/plugins/specs/ui/which-key.lua
+++ nvim/.config/nvim/lua/plugins/specs/ui/which-key.lua
@@ -213,7 +213,6 @@ return {
 					"<cmd>Telescope diagnostics<cr>",
 					"Workspace Diagnostics",
 				},
-				f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
 				i = { "<cmd>LspInfo<cr>", "Info" },
 				I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
 				j = {
