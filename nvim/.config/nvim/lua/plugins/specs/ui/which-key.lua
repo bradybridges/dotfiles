6e1c82ae448726ae3be7424fbbfd6568af915cf1
brady bridges <bradyjbridges@gmail.com>
2024-06-15 00:42:20 -0500

    Revert "Remove format keybind"
    This reverts commit 714e87523f34e5ea01074242971f7ef73bf58117.

--- nvim/.config/nvim/lua/plugins/specs/ui/which-key.lua
+++ nvim/.config/nvim/lua/plugins/specs/ui/which-key.lua
@@ -213,6 +213,7 @@ return {
 					"<cmd>Telescope diagnostics<cr>",
 					"Workspace Diagnostics",
 				},
+				f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
 				i = { "<cmd>LspInfo<cr>", "Info" },
 				I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
 				j = {
