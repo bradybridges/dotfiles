bc70e0f2050091fac022a52f76d3a9862cdc799a
brady <bradyjbridges@gmail.com>
2024-11-21 13:01:29 -0600

    Disable copilot tab completion, use <C-c> instead to avoid conflictions

--- nvim/.config/nvim/lua/plugins/specs/editor/copilot.lua
+++ nvim/.config/nvim/lua/plugins/specs/editor/copilot.lua
@@ -2,4 +2,14 @@ return {
 	"github/copilot.vim",
 	cmd = "Copilot",
 	event = "InsertEnter",
+	init = function()
+		vim.g.copilot_workspace_folders = "~/projects/snap/"
+		vim.g.copilot_no_tab_map = true
+
+		vim.keymap.set("i", "<C-c>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+			desc = "Accept copilot completion",
+		})
+	end,
 }
