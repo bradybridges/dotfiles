ebd5ec52e201f890b5a397336b4d4c1fdf5e7d0a
brady bridges <bradyjbridges@gmail.com>
2025-07-24 22:30:09 -0500

    Add autocmd to highlight yanked text

--- global/nvim/.config/nvim/lua/config/autocmds.lua
+++ global/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
+-- Highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 -- Set shiftwidth to 4 and disable expandtab for css and scss files
 vim.cmd([[
   autocmd FileType css,scss setlocal shiftwidth=4 noexpandtab
