81fb069dd14f04b5e4e81f9a48d8618ad0e8c89b
brady <bradyjbridges@gmail.com>
2025-08-01 11:20:31 -0500

    Add formatter configs for commonly used filetypes

--- global/nvim/.config/nvim/lua/plugins/conform.lua
+++ global/nvim/.config/nvim/lua/plugins/conform.lua
@@ -18,10 +18,18 @@ return {
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
+
 				-- You can customize some of the format options for the filetype (:help conform.format)
 				rust = { "rustfmt", lsp_format = "fallback" },
+
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				scss = { "prettierd", "prettier", stop_after_first = true },
 			},
 		})
 	end,
