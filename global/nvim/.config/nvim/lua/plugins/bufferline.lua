0351b83e437478b570219cded0be8ef3b7622784
brady <bradyjbridges@gmail.com>
2025-10-14 09:20:18 -0500

    Update close commands to use snacks.bufdelete

--- global/nvim/.config/nvim/lua/plugins/bufferline.lua
+++ global/nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -15,8 +15,12 @@ return {
 				right_trunc_marker = "",
 				max_name_length = 30,
 				max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-				close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-				right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+				close_command = function(bufnum)
+					require("snacks").bufdelete(bufnum)
+				end, -- can be a string | function, see "Mouse actions"
+				right_mouse_command = function(bufnum)
+					require("snacks").bufdelete(bufnum)
+				end, -- can be a string | function, see "Mouse actions"
 				left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
 				diagnostics = "nvim_lsp",
 				always_show_bufferline = false,
