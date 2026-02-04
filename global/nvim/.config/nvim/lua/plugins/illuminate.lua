40b54b5cdd9b6dfa4bd68ff74cb1dc17597ec25a
brady <bradyjbridges@gmail.com>
2026-02-04 15:49:48 -0600

    Revert "Add allowlist config"
    This reverts commit 7b09b332c7aea21853ffe329fc031fdb94bddc95.

--- global/nvim/.config/nvim/lua/plugins/illuminate.lua
+++ global/nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,14 +1,4 @@
 return {
 	"RRethy/vim-illuminate",
 	event = "InsertEnter",
-	opts = {
-		filetypes_allowlist = {
-			"css",
-			"html",
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact"
-		}
-	},
 }
