7b09b332c7aea21853ffe329fc031fdb94bddc95
brady <bradyjbridges@gmail.com>
2026-02-04 15:49:23 -0600

    Add allowlist config

--- global/nvim/.config/nvim/lua/plugins/illuminate.lua
+++ global/nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,4 +1,14 @@
 return {
 	"RRethy/vim-illuminate",
 	event = "InsertEnter",
+	opts = {
+		filetypes_allowlist = {
+			"css",
+			"html",
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact"
+		}
+	},
 }
