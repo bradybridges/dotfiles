a5ecc00b6121b45986ffb482f4bd22f87cfd581a
bradybridges <bradyjbridges@gmail.com>
2026-03-20 11:32:25 -0500

    Update theme name after breaking change in catppuccin theme

--- global/nvim/.config/nvim/lua/plugins/lualine.lua
+++ global/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -39,7 +39,7 @@ return {
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				theme = "catppuccin",
+				theme = "catppuccin-nvim",
 				component_separators = { left = "|", right = "|" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
