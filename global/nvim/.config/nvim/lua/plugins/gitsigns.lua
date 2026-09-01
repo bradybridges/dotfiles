df7a352f805055f84ed0bfe3b0295d694b21b58e
Brady - MB Work <bradyjbridges@gmail.com>
2026-09-01 11:23:50 -0500

    Add mappings to jump to prev/next hunk

--- global/nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ global/nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,31 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			map("n", "]c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					gitsigns.nav_hunk("next")
+				end
+			end, { desc = "Next git hunk" })
+
+			map("n", "[c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					gitsigns.nav_hunk("prev")
+				end
+			end, { desc = "Previous git hunk" })
+		end,
 		signs = {
 			add = { text = "┃" },
 			change = { text = "┃" },
